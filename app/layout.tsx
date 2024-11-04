b6cbe66fcf3a79469764b2914acaa3a1dea778f0
bibhabasum <bibhabasumohapatrabm@gmail.com>
2024-11-05 01:52:11 +0530

    stable- icon add, theme-switch-nav.tsx-changing-theme with toggle is left-v2

--- app/layout.tsx
+++ app/layout.tsx
@@ -53,15 +53,6 @@ export default function RootLayout({
         GeistMono.variable
       )}
     >
-      <body className="antialiased max-w-xl mx-4 mt-8 lg:mx-auto">
-        <main className="flex-auto min-w-0 mt-6 flex flex-col px-2 md:px-0">
-          <Navbar />
-          {children}
-          <Footer />
-          <Analytics />
-          <SpeedInsights />
-        </main>
-      </body>
       <body className="antialiased flex flex-col items-center justify-center mx-auto mt-2 lg:mt-8 mb-20 lg:mb-40">
         <ThemeProvider
           attribute="class"
